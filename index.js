1c8c717ec798e19c56a140c9c18d809bf87fb993
DrRambleDev <153151906+DrRambleDev@users.noreply.github.com>
2024-03-14 12:23:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -33,4 +33,25 @@ const enhance = id => {
   });
 }
 
+document.getElementById("effect2").onmouseover = event => {
+  let iteration = 0;
+
+  const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+      .map((letter, index) => {
+
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 35);
+}
 enhance("channel-link");
