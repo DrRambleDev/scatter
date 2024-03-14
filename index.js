4632aa79b0f79e68688b7b2129d2da39f63dff47
DrRambleDev <153151906+DrRambleDev@users.noreply.github.com>
2024-03-14 12:27:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
+// Variables (global, because why not?)
+
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const letters = "QAZWSXEDCRFVTGBYHNUJMIKOLP";
+
+// Functions
 
 const enhance = id => {
-  const element = document.getElementById(id),
-        text = element.innerText.split("");
-  
+  const element = document.getElementById(id);
+  const text = element.innerText.split("");
+
   element.innerText = "";
-  
+
   text.forEach((value, index) => {
     const outer = document.createElement("span");
-    
+
     outer.className = "outer";
-    
+
     const inner = document.createElement("span");
-    
+
     inner.className = "inner";
-    
+
     inner.style.animationDelay = `${rand(-5000, 0)}ms`;
-    
+
     const letter = document.createElement("span");
-    
+
     letter.className = "letter";
-    
+
     letter.innerText = value;
-    
-    letter.style.animationDelay = `${index * 1000 }ms`;
-    
-    inner.appendChild(letter);    
-    
-    outer.appendChild(inner);    
-    
+
+    letter.style.animationDelay = `${index * 1000}ms`;
+
+    inner.appendChild(letter);
+
+    outer.appendChild(inner);
     element.appendChild(outer);
   });
 }
 
-document.getElementById("effect2").onmouseover = event => {
+document.getElementById("tree").onmouseover = event => {
   let iteration = 0;
 
   const interval = setInterval(() => {
@@ -54,4 +58,7 @@ document.getElementById("effect2").onmouseover = event => {
     iteration += 1 / 3;
   }, 35);
 }
+
+// Declare functions
+
 enhance("channel-link");
